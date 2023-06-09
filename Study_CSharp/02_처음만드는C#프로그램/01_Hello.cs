fe51e60508b796017931dcdd2f5ff04493dcf5ea
MinJoon Kim <kmj73203@naver.com>
2023-06-09 15:07:52 +0900

    초기 커밋.

---
+++ Study_CSharp/02_처음만드는C#프로그램/01_Hello.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+namespace Study_CSharp._02_처음만드는C_프로그램
+{
+    internal class _01_Hello
+    {
+        static void Main(string[] args)
+        {
+            String str = Console.ReadLine();
+            WriteLine("Hello, {0}!", str);
+        }
+    }
+}
